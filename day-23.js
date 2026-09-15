c6b05f7ae91e433070d82dd183351944933e15ac
satyam maurya <satyammauryas368@gmail.com>
2026-09-15 21:09:25 +0530

    Day 23 : Introduction in Javascript, Variables, Datatypes & Object.

---
+++ day-23.js
@@ -0,0 +1,40 @@
+console.log("hello Javascript");
+let age = 15;
+age = 16;
+const country = "India";
+var name = "Rahul";
+console.log(name);
+console.log(age);
+console.log(country);
+let studentName = "Aman";
+let marks = 95;
+let isStudent = true;
+let school;
+let result = null;
+let bigNumber = 12345678995123n;
+let id = Symbol("id");
+console.log(studentName);
+console.log(marks);
+console.log(isStudent);
+console.log(school);
+console.log(result);
+console.log(bigNumber);
+console.log(id);
+console.log(typeof studentName);
+console.log(typeof marks);
+console.log(typeof isStudent);
+console.log(typeof school);
+let student = {
+    name: "Aman",
+    age:16,
+    class: 11,
+    isPassed: true
+};
+console.log(student.name);
+console.log(student.age);
+console.log(student.class);
+student.age = 17;
+console.log(student.age);
+student.city = "Lucknow";
+console.log(student);
+
