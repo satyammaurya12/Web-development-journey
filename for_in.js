4bf9d50c42aa5b4140a7f199dbd02ff145eb1bd8
satyam maurya <satyammauryas368@gmail.com>
2026-09-17 21:50:09 +0530

    Day 24 : Conditional & Loops in Javascript

---
+++ for_in.js
@@ -0,0 +1,4 @@
+let student = { name: "satyam", age: 19 };
+for(let key in student){
+    console.log(key,student[key]);
+}
---
+++ ifelseladder.js
@@ -0,0 +1,10 @@
+let marks = 75;
+if(marks>=90){
+    console.log("Grade");
+}
+else if(marks>=60){
+    console.log("Grade B");
+}
+else{
+    console.log("Grade c")
+}
